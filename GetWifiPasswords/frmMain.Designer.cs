c6fa80c80a84f07d7d6fe56ba47533ffe5fea883
Manjoor Ali <manjoor@live.com>
2023-04-04 19:18:38 +0530

    Add project files.

---
+++ GetWifiPasswords/frmMain.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace GetWifiPasswords
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvList = new System.Windows.Forms.ListView();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lvList
+            // 
+            this.lvList.HideSelection = false;
+            this.lvList.Location = new System.Drawing.Point(12, 12);
+            this.lvList.Name = "lvList";
+            this.lvList.Size = new System.Drawing.Size(460, 417);
+            this.lvList.TabIndex = 0;
+            this.lvList.UseCompatibleStateImageBehavior = false;
+            this.lvList.DoubleClick += new System.EventHandler(this.lvList_DoubleClick);
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.BackColor = System.Drawing.Color.Transparent;
+            this.lblLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoading.Location = new System.Drawing.Point(116, 179);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(217, 25);
+            this.lblLoading.TabIndex = 1;
+            this.lblLoading.Text = "Loading.. Please wait";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 441);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.lvList);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 480);
+            this.Name = "frmMain";
+            this.Text = "Wifi Password";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvList;
+        private System.Windows.Forms.Label lblLoading;
+    }
+}
+
---
+++ GetWifiPasswords/frmMain.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetWifiPasswords
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lvList.View = View.Details;
+            lvList.Columns.Add("SSID");
+            lvList.Columns.Add("Password");
+            lvList.Columns[0].Width= (lvList.Width / 2) - 20;
+            lvList.Columns[1].Width = (lvList.Width / 2) - 20;
+            lvList.FullRowSelect = true;          
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            foreach (string ssid in getSSIDs())
+            {
+                string pwd = getPassword(ssid);
+                ListViewItem item = new ListViewItem(ssid);
+                item.SubItems.Add(pwd);
+                lvList.Items.Add(item);
+            }
+            lblLoading.Visible = false;
+        }
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            lvList.Width = this.Width - 50;
+            lvList.Height = this.Height - 65;
+            lvList.Columns[0].Width = (lvList.Width / 2) - 20;
+            lvList.Columns[1].Width = (lvList.Width / 2) - 20;
+        }
+        private List<string> getSSIDs()
+        {
+            List<string> list = new List<string>();
+            string output = "";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "netsh";
+            process.StartInfo.Arguments = "wlan show profiles";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            output = process.StandardOutput.ReadToEnd();
+            foreach (var s in output.Split('\n'))
+            {
+                if(s.IndexOf("   :") > 5)
+                {
+                    var ssid = s.Substring(s.IndexOf(":") +1).Replace("\r","").Trim();
+                    list.Add(ssid);
+                }                
+            }
+
+            process.WaitForExit();
+            return list;
+        }
+        private string getPassword(string ssid)
+        {
+            string output = "";
+            string pwd = "";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "netsh";
+            process.StartInfo.Arguments = "wlan show profile name=\"" + ssid + "\" key=clear";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            output = process.StandardOutput.ReadToEnd();
+            if (output.IndexOf("Key Content") > 2)
+            {
+                int s = output.IndexOf("Key Content") + 11;
+                output = output.Substring(s);
+                int e = output.IndexOf("\r");
+                output = output.Substring(2, e-1);
+                pwd = output.Replace("\r", "").Trim().Replace(":","");
+            }
+            
+            process.WaitForExit();
+            return pwd;
+        }
+
+        private void lvList_DoubleClick(object sender, EventArgs e)
+        {
+            if(lvList.SelectedIndices.Count == 1)
+                Clipboard.SetText(lvList.SelectedItems[0].SubItems[1].Text);
+        }
+    }
+}
